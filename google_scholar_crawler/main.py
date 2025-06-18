4437d89bed1d75af7d0343257bf7b10d6ab33010
DuanWei <duanwei@hhu.edu.cn>
2025-06-18 14:14:25 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,20 +1,13 @@
-from scholarly import scholarly, ProxyGenerator
+from scholarly import scholarly
 import json
 from datetime import datetime
 import os
 
-# ✅ 启用代理绕过封锁，注意传入 repeat 参数
-pg = ProxyGenerator()
-pg.FreeProxies(repeat=2)
-scholarly.use_proxy(pg)
-
-# 🔍 Scholar 抓取逻辑
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+author = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']: v for v in author['publications']}
 
-# 📝 保存结果
 os.makedirs('results', exist_ok=True)
 with open('results/gs_data.json', 'w') as f:
     json.dump(author, f, ensure_ascii=False)
